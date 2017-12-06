d4e1138a6b6eb54f1f1f7366a1f2c9c51ef10075
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-12-05 22:56:19 -0300

    Slider com ViewPager

---
+++ app/src/main/java/com/dellpc/slide/MainActivity.java
@@ -0,0 +1,22 @@
+package com.dellpc.slide;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Instanciamos o ViewPager e PagerAdapter
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
+    }
+}
